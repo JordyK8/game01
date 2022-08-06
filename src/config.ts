6731438f1b6df2d09d4f34ef6c1d65c60d3624ed
Jordy Kokelaar <jordykokelaar@gmail.com>
2022-08-06 08:13:01 +0200

    bomberman v1

--- src/config.ts
+++ src/config.ts
@@ -2,12 +2,19 @@ import Phaser from 'phaser';
 
 export default {
   type: Phaser.AUTO,
+  physics: {
+    default: 'arcade',
+    
+    arcade: {
+      debug: true,
+    }
+  },
   parent: 'game',
   backgroundColor: '#33A5E7',
   scale: {
-    width: 800,
-    height: 600,
+    width: 480,
+    height: 853,
     mode: Phaser.Scale.FIT,
-    autoCenter: Phaser.Scale.CENTER_BOTH
+    autoCenter: Phaser.Scale.CENTER_BOTH,
   }
 };
--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,24 +1,83 @@
-import Phaser from 'phaser';
+import Phaser, { GameObjects } from 'phaser';
 
 export default class Demo extends Phaser.Scene {
+  private player: GameObjects.Rectangle;
+  private cursors: Phaser.Types.Input.Keyboard.CursorKeys;
+  private bombs: number;
+  private bomb: GameObjects.Sprite;
+  private maxBombs: number;
   constructor() {
     super('GameScene');
+    this.player;
+    this.cursors;
+    this.bombs = 0;
+    this.maxBombs = 1;
+    this.bomb;
   }
 
   preload() {
-    this.load.image('logo', 'assets/phaser3-logo.png');
+    this.load.spritesheet('bomb', 'assets/bomb.png', {
+      frameWidth: 20,
+      frameHeight: 26,
+      startFrame: 0,
+      endFrame: 5});
+    this.load.spritesheet('bomb', 'assets/bomb.png', {
+      frameWidth: 20,
+      frameHeight: 26,
+      startFrame: 0,
+      endFrame: 5});
   }
-
-  create() {
-    const logo = this.add.image(400, 70, 'logo');
-
-    this.tweens.add({
-      targets: logo,
-      y: 350,
-      duration: 1500,
-      ease: 'Sine.inOut',
-      yoyo: true,
-      repeat: -1
-    });
+  create ()
+  {
+      this.player = this.add.rectangle(400, 300, 20, 20, 0xffffff);
+      this.bomb = this.add.sprite(0,0, 'bomb')
+      this.cursors = this.input.keyboard.createCursorKeys();
+      this.physics.add.existing(this.player, false);
+    this.anims.create({
+      key: 'bomb',
+      frames: this.anims.generateFrameNumbers('bomb', {
+        frames: [0, 1, 2, 3, 4, 5]
+      }),
+      frameRate: 1.6,
+      repeat: 0
+      })
+      
+      this.player.body.setCollideWorldBounds(true);
   }
+  
+  update ()
+  {
+      this.player.body.setVelocity(0);
+  
+      if (this.cursors.left.isDown)
+      {
+          this.player.body.setVelocityX(-100);
+      }
+      else if (this.cursors.right.isDown)
+      {
+          this.player.body.setVelocityX(100);
+      }
+  
+      if (this.cursors.up.isDown)
+      {
+          this.player.body.setVelocityY(-100);
+      }
+      else if (this.cursors.down.isDown)
+      {
+          this.player.body.setVelocityY(100);
+      }
+      
+    this.cursors.space.once('down', () => {
+      if (this.bombs < this.maxBombs) {
+        this.bombs++;
+        const bomb = this.add.sprite(this.player.x, this.player.y , 'bomb')
+        bomb.play('bomb')
+        setTimeout(() => {
+          bomb.destroy();
+          this.bombs--;
+        }, 4000);
+      }
+    })
+  }
+  
 }
